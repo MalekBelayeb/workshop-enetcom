95f691443d426864d9e9a61cdfcc2db5418b7c86
Ahmed abdmouleh <108586834+ahmedabdmouleh8@users.noreply.github.com>
2022-12-21 16:48:10 +0100

    feat: create page detail

--- Workshop Enetcom/Workshop Enetcom/PlaceDetailViewController.swift
+++ Workshop Enetcom/Workshop Enetcom/PlaceDetailViewController.swift
@@ -9,11 +9,19 @@ import UIKit
 
 class PlaceDetailViewController: UIViewController {
 
+    @IBOutlet weak var descriptionLabel: UILabel!
     @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var textDesciption: UITextView!
+    @IBOutlet weak var lableLable: UILabel!
+    @IBOutlet weak var imagePlace: UIImageView!
+    var place : PlaceModel?
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        self.titleLabel.text = place?.title
+        self.lableLable.text = place?.categorie
+        self.textDesciption.text = place?.distance
+        self.imagePlace.image = UIImage(named: place?.image ?? "")
         // Do any additional setup after loading the view.
     }
     
